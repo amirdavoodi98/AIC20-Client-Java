0f17e2d742dcc9da57da4bc942835864db4c8757
aria saeidi <aria.saeidi98@gmail.com>
2020-02-21 15:57:25 +0330

    implement some method for analyzing the map

--- src/Client/constants/ConstantsValue.java
+++ src/Client/constants/ConstantsValue.java
@@ -1,5 +1,5 @@
 package Client.constants;
 
 public class ConstantsValue {
-    public static final int[] STARTER_UNITS = new int[]{0, 5, 4, 1, 2};
+    public static final int[] STARTER_UNITS = new int[]{0, 8, 6, 2, 7};
 }
--- src/Client/utils/Utils.java
+++ src/Client/utils/Utils.java
@@ -1,9 +1,12 @@
 package Client.utils;
 
 import Client.Model.BaseUnit;
+import Client.Model.Map;
+import Client.Model.Unit;
 import Client.Model.World;
 import Client.constants.ConstantsValue;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
 
@@ -25,7 +28,7 @@ public class Utils {
         return list;
     }
 
-    public static void chooseSpecificHand(World world){
+    public static void chooseSpecificHand(World world) {
         List<BaseUnit> allBaseUnits = world.getAllBaseUnits();
         List<BaseUnit> starterHand = new ArrayList<>();
         List<Integer> starterUnitList = Utils.toList(ConstantsValue.STARTER_UNITS);
@@ -69,5 +72,91 @@ public class Utils {
             System.out.println("selected units: " + baseUnit.getTypeId());
         });
         world.chooseHand(starterHand);
+    }
+
+    public static List<Unit> getUnits(World world, int row, int col) {
+        return getUnits(world.getMap(), row, col);
+    }
+
+    public static List<Unit> getUnits(Map map, int row, int col) {
+        return map.getCell(row, col).getUnits();
+    }
+
+    public static HashMap<Pair<Integer, Integer>, List<Unit>> getAllPresentUnits(World world) {
+        HashMap<Pair<Integer, Integer>, List<Unit>> allPresentUnits = new HashMap<Pair<Integer, Integer>, List<Unit>>();
+        Map map = world.getMap();
+        for (int row = 0; row < map.getRowNum(); row++) {
+            for (int col = 0; col < map.getColNum(); col++) {
+                List<Unit> units = getUnits(map, row, col);
+                if (units.size() != 0) {
+                    allPresentUnits.put(new Pair<Integer, Integer>(row, col), units);
+                }
+            }
+        }
+        return allPresentUnits;
+    }
+
+    public static HashMap<Pair<Integer, Integer>, List<Unit>> getUnitsByPlayerId(World world, int playerId) {
+        HashMap<Pair<Integer, Integer>, List<Unit>> presentUnits = new HashMap<Pair<Integer, Integer>, List<Unit>>();
+        Map map = world.getMap();
+
+        for (int row = 0; row < map.getRowNum(); row++) {
+            for (int col = 0; col < map.getColNum(); col++) {
+                List<Unit> units = getUnits(map, row, col);
+                if (units.size() != 0) {
+                    List<Unit> result = new ArrayList<Unit>();
+                    units.forEach(unit -> {
+                        if (unit.getPlayerId() == playerId) {
+                            result.add(unit);
+                        }
+                    });
+                    if (result.size() != 0) {
+                        presentUnits.put(new Pair<Integer, Integer>(row, col), result);
+                    }
+                }
+            }
+        }
+        return presentUnits;
+    }
+
+    public static HashMap<Pair<Integer, Integer>, List<Unit>> getMyUnits(World world) {
+        return getUnitsByPlayerId(world, world.getMe().getPlayerId());
+    }
+
+    public static HashMap<Pair<Integer, Integer>, List<Unit>> getFriendUnits(World world) {
+        return getUnitsByPlayerId(world, world.getFriend().getPlayerId());
+    }
+
+    public static HashMap<Pair<Integer, Integer>, List<Unit>> getFirstEnemyUnits(World world) {
+        return getUnitsByPlayerId(world, world.getFirstEnemy().getPlayerId());
+    }
+
+    public static HashMap<Pair<Integer, Integer>, List<Unit>> getSecondEnemyUnits(World world) {
+        return getUnitsByPlayerId(world, world.getSecondEnemy().getPlayerId());
+    }
+
+    public static HashMap<Pair<Integer, Integer>, List<Unit>> getEnemyUnits(World world) {
+        HashMap<Pair<Integer, Integer>, List<Unit>> presentUnits = new HashMap<Pair<Integer, Integer>, List<Unit>>();
+        Map map = world.getMap();
+        int firstEnemyId = world.getFirstEnemy().getPlayerId();
+        int secondEnemyId = world.getSecondEnemy().getPlayerId();
+
+        for (int row = 0; row < map.getRowNum(); row++) {
+            for (int col = 0; col < map.getColNum(); col++) {
+                List<Unit> units = getUnits(map, row, col);
+                if (units.size() != 0) {
+                    List<Unit> result = new ArrayList<Unit>();
+                    units.forEach(unit -> {
+                        if (unit.getPlayerId() == firstEnemyId || unit.getPlayerId() == secondEnemyId) {
+                            result.add(unit);
+                        }
+                    });
+                    if (result.size() != 0) {
+                        presentUnits.put(new Pair<Integer, Integer>(row, col), result);
+                    }
+                }
+            }
+        }
+        return presentUnits;
     }
 }
